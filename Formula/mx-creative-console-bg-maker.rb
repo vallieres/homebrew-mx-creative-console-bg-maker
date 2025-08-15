2e462a83cb9338dc2976cf107c78ab52a08a7f66
Alexandre Vallières-Lagacé <alexandre.valliereslagace@docker.com>
2025-08-15 15:26:34 -0400

    Updated version

--- Formula/mx-creative-console-bg-maker.rb
+++ Formula/mx-creative-console-bg-maker.rb
@@ -1,8 +1,8 @@
 class MxCreativeConsoleBgMaker < Formula
   desc "A command-line tool that splits images into a 3x3 grid for the Logitech MX Creative Console"
   homepage "https://github.com/vallieres/mx-creative-console-bg-maker"
-  url "https://github.com/vallieres/mx-creative-console-bg-maker/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "your-sha256-hash-here"
+  url "https://github.com/vallieres/mx-creative-console-bg-maker/archive/refs/tags/2025.2.tar.gz"
+  sha256 "8d755bcb9461f99b44d751a230b8fa83dbbf1f0fef4b96845962113f25a26ca8"
   license "MIT"
   
   depends_on "go" => :build
@@ -13,7 +13,7 @@ class MxCreativeConsoleBgMaker < Formula
   
   test do
     # Test that installation worked - since it requires an image file, just check help
-    output = shell_output("#{bin}/ccbm 2>&1", 1)
-    assert_match "Usage: imgsplit <image_path>", output
+    output = shell_output("#{bin}/ccbm --help2>&1", 1)
+    assert_match "Usage: ccbm <image_path>", output
   end
 end
